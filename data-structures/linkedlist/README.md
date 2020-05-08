48b9b62ed33d02bd00f3b1712c7873f631800f0d
Leonardo Gonçalves <ldsz@gft.com>
2020-05-08 20:03:09 -0300

    [WIP] Linked list

---
+++ data-structures/linkedlist/README.md
@@ -0,0 +1,23 @@
+# Linked List
+
+## What is a linked list
+
+A linked list is a sequential list of  nodes that hold data which point to other nodes also containing data.
+
+## Where are linked lists used?
+
+- Used in many List, Queue & Stack implementations.
+
+- Great for creating circular lists.
+- Can easily model real world objects such as trains.
+- Used in separate chaining which is present certain Hashtable implementations to deal with hashing collisions.
+- Often used in the implementation of adjacency lists for graphs.
+
+## Complexity Analysis
+
+- Search: O(n)
+- Insert at head: O(1)
+- Insert at tail: O(1)
+- Remove at head: O(1)
+- Remove at tail: O(n)
+- Remove in middle: O(n)
---
+++ data-structures/linkedlist/linkedlist.go
@@ -0,0 +1,79 @@
+package linkedlist
+
+import (
+	"fmt"
+)
+
+// Node is a Node of LinkedList
+type Node struct {
+	data int
+	next *Node
+}
+
+func (n *Node) String() string {
+	return fmt.Sprintf("%v", n.data)
+}
+
+type LinkedList struct {
+	head *Node
+	tail *Node
+	size int
+}
+
+func (ll *LinkedList) Head() int {
+	return ll.head.data
+}
+
+// Clear removes all elements from linked list, O(n)
+func (ll *LinkedList) Clear() {
+	trav := ll.head
+	for trav != nil {
+		next := trav.next
+		trav.next = nil
+		trav.data = 0
+		trav = next
+	}
+	ll.head, ll.tail, trav = nil, nil, nil
+	ll.size = 0
+}
+
+// Size returns the total the elements of the linked list
+func (ll *LinkedList) Size() int {
+	return ll.size
+}
+
+// IsEmpty checks if the linked list is empty
+func (ll *LinkedList) IsEmpty() bool {
+	return ll.size == 0
+}
+
+// Append puts the element on tail of the linked list
+func (ll *LinkedList) Append(data int) {
+	n := &Node{data, nil}
+	if ll.IsEmpty() {
+
+		ll.head, ll.tail = n, n
+		ll.size++
+		return
+	}
+
+	ll.tail.next = n
+	ll.tail = ll.tail.next
+	ll.size++
+
+}
+
+// Prepend puts the element on head of the linked list
+func (ll *LinkedList) Prepend(data int) {
+
+	if ll.IsEmpty() {
+		ll.head, ll.tail = n, n
+		ll.size++
+		return
+	}
+
+	head := ll.head
+	ll.head = n
+	ll.head.next = head
+	ll.size++
+}
